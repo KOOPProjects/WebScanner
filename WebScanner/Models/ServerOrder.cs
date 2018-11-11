a95ba8e30d772b3c4226c561cfd8b4319e1424cb
OskarPrzybylski <Przybylski.oskar@gmail.com>
2018-11-11 18:02:54 +0100

    Added models for managing Orders in Db

---
+++ WebScanner/Models/ServerOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebScanner.Models
+{
+    public class ServerOrder : Order
+    {
+        public string Question { get; set; }
+    }
+}
